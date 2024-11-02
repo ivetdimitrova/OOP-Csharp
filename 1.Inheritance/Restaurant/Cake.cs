158f18ea41dc0379895f2ce257e9353e29e9e8e2
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-11-02 14:15:38 +0200

    Add

---
+++ 1.Inheritance/Restaurant/Cake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class Cake :Dessert
+    {
+        private const double Grams = 250;
+        private const double Calories = 1000;
+        private const decimal Price = 5m;
+        public Cake(string name) : base(name, Price, Grams, Calories)
+        {
+        }
+    }
+}

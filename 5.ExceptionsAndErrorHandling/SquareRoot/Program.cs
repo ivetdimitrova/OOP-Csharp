40e66f12479646a29e91d234b39763173c2a0c29
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-11-10 15:57:51 +0200

    Add

---
+++ 5.ExceptionsAndErrorHandling/SquareRoot/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = int.Parse(Console.ReadLine());
+
+            try
+            {
+                double squareRoot = CalculateSquareRoot(number);
+
+                Console.WriteLine(squareRoot);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+            
+        }
+
+        private static double CalculateSquareRoot(double number)
+        {
+            if (number < 0)
+                throw new ArgumentException("Invalid number.");
+
+
+            return Math.Sqrt(number);
+        }
+    }
+}
---
+++ 5.ExceptionsAndErrorHandling/SumOfIntegers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Data;
+
+namespace SumOfIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+            int integersSum = CalculateIntegersSum(input);
+            Console.WriteLine($"The total sum of all integers is: {integersSum}");
+        }
+
+        private static int CalculateIntegersSum(string[] input)
+        {
+            int sum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                string element = input[i];
+
+                try
+                {
+                    bool isInteger = long.TryParse(element, out long number);
+
+                    if (number < int.MinValue || number > int.MaxValue)
+                        throw new OverflowException($"The element '{element}' is out of range!");
+
+                    if (!isInteger)
+                        throw new FormatException($"The element '{element}' is in wrong format!");
+
+                    sum += (int)number;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{element}' processed - current sum: {sum}");
+                }
+            }
+
+            return sum;
+        }
+    }
+}

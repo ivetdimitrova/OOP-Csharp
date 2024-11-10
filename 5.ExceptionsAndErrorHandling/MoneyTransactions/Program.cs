24fb3c638acd56b691843ed3ae2bc2d48d22d335
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-11-10 15:56:47 +0200

    Add

---
+++ 5.ExceptionsAndErrorHandling/MoneyTransactions/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Security.Principal;
+
+namespace MoneyTransactions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bankAccounts = Console.ReadLine().Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, double> balanceByAccount = new Dictionary<string, double>();
+
+            for (int i = 0; i < bankAccounts.Length; i++)
+            {
+                string[] account = bankAccounts[i].Split('-',StringSplitOptions.RemoveEmptyEntries);
+
+                balanceByAccount.Add(account[0], double.Parse(account[1]));
+            }
+
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] command = input.Split();
+
+                string action = command[0];
+                string account = command[1];
+                double sum = double.Parse(command[2]);
+
+                try
+                {
+                    switch (action)
+                    {
+                        case "Deposit":
+
+                            if (IsValidAccount(account, balanceByAccount))
+                                balanceByAccount[account] += sum;
+                            break;
+                        case "Withdraw":
+                            if (IsValidAccount(account, balanceByAccount) &&
+                                IsLowerThanBalance(sum, balanceByAccount[account]))
+                                balanceByAccount[account] -= sum;
+                            break;
+                        default:
+                            throw new ArgumentException("Invalid command!");
+
+                    }
+
+                    Console.WriteLine($"Account {account} has new balance: {balanceByAccount[account]:f2}");
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.WriteLine("Enter another command");
+            }
+        }
+
+        private static bool IsLowerThanBalance(double sum, double balance)
+        {
+            if (sum > balance)
+            {
+                throw new ArgumentException("Insufficient balance!");
+                return false;
+            }
+               
+            return true;
+        }
+
+        private static bool IsValidAccount(string account, Dictionary<string, double> balanceByAccount)
+        {
+            if (!balanceByAccount.ContainsKey(account))
+            {
+                throw new ArgumentException("Invalid account!");
+                return false;
+            }
+
+
+            return true;
+        }
+    }
+}
---
+++ 5.ExceptionsAndErrorHandling/PlayCatch/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace PlayCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int exceptionCount =0;
+            while (exceptionCount < 3)
+            {
+                string[] command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                int index = 0;
+                try
+                {
+                    switch (command[0])
+                    {
+                            
+                        case "Replace":
+                             index = IsValidIndex(command[1],numbers);
+                            int element = IsValidElement(command[2]);
+
+                            numbers[index] = element;
+
+                            break;
+                        case "Show": 
+                            index = IsValidIndex(command[1], numbers);
+
+                            Console.WriteLine(numbers[index]);
+                            break;
+                        case "Print":
+                            index = IsValidIndex(command[1], numbers);
+                            int endindex = IsValidIndex(command[2], numbers);
+
+                            Print(numbers, index, endindex);
+                            break;
+
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    exceptionCount++;
+                }
+                catch (FormatException ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                    exceptionCount++;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+
+        private static void Print(int[] numbers, int startIndex, int endIndex)
+        {
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                if (i == endIndex)
+                {
+                    Console.Write(numbers[i]);
+                    break;
+                }
+                
+                Console.Write(numbers[i] + ", ");
+            }
+            
+            Console.WriteLine();
+        }
+
+        private static int IsValidElement(string e)
+        {
+            if (!int.TryParse(e, out int elment))
+                throw new FormatException("The variable is not in the correct format!");
+
+            return elment;
+        }
+
+        private static int IsValidIndex(string i, int[] numbers)
+        {
+            if (!int.TryParse(i, out int index))
+                throw new FormatException("The variable is not in the correct format!");
+
+            if (index < 0 || index >= numbers.Length)
+                throw new ArgumentException("The index does not exist!");
+
+
+            return index;
+        }
+    }
+}
